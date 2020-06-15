fee7cb5c4f97cd4ea66af43452cddf399a0e36c6
minkj1992 <minkj1992@gmail.com>
2020-06-15 17:55:23 +0900

    feat: Update hello.py with flask-script

--- src/ch01/hello.py
+++ src/ch01/hello.py
@@ -1,5 +1,8 @@
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
+manager = Manager(app)
 
 
 @app.route('/')
@@ -11,4 +14,5 @@ def user(name):
     return '<h1>Hello, %s!</h1>' % name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
+
