6baedaea6fa0761c93f4649c67394abefafbb745
minkj1992 <minkj1992@gmail.com>
2020-06-18 22:44:56 +0900

    Add User Domain with Flask-SQLAlchemy

--- app/runserver.py
+++ app/runserver.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
@@ -5,13 +6,41 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_wtf.csrf import CSRFProtect
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess secret key'  # 전역적으로 사용하는 secret key
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 csrf = CSRFProtect(app)  # wtf에서 제공하는 FlaskForm을 사용하지 않을 경우 global하게 csrf 보호
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    # User 모델에 role 속성을 추가, 관계의 반대 방향을 정의, Role 모델이 외래키 대신 오브젝트에 접근하도록 role_id 대신 사용
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 
 class NameForm(FlaskForm):
@@ -35,9 +64,10 @@ def index():
     if form.validate_on_submit():  # return bool(request) and request.method in SUBMIT_METHODS
         legacy_name = session.get('name')
         if legacy_name != form.name.data:
-            flash('Your name is changed', category="success")
+            flash('Your name is changed', category="success")  # Jinja2에 넘겨진다, bootstrap alert class이름에 category명은 종속적
             session['name'] = form.name.data
         else:
             flash('Looks like you already have that name!', category="warning")
-        return redirect(url_for('index')) # Post/Redirect/Get (PRG) pattern
-    return render_template('index.html', form=form, name=session.get('name')) # .get()은 찾을 수 없는 경우 None을 return하여 Error 방지
+        return redirect(url_for('index'))  # Post/Redirect/Get (PRG) pattern
+    return render_template('index.html', form=form,
+                           name=session.get('name'))  # .get()은 찾을 수 없는 경우 None을 return하여 Error 방지
